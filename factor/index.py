9a0fd9d5719c63b9053ad50ad12d6bb1a736389e
luozero@126.com <luozero@126.com>
2024-08-04 12:55:22 +0000

    thiere is a bug

--- factor/index.py
+++ factor/index.py
@@ -10,17 +10,18 @@ class index:
   def calc(self):
       dict_index = {}
       for sector in self.sectors:
-          symbols = us_dir0_load_csv(dir0 = 'symbol', filename= sector +'.csv')['symbol'].values
-          data = us_equity_daily_data_load(symbols = symbols, start_date = self.start_date,
-                                           end_date = self.end_date, trade_option = 'market_capital', 
-                                           dir_option = 'xq')
-          df = pd.DataFrame(data)
-          df_sum = df.sum(axis=1)
-          index = df_sum * 1000 /df_sum.iloc[0]
-          dict_index[sector] = index
+        print(f"processing {sector} ...")
+        symbols = us_dir1_load_csv(dir0 = 'symbol', dir1 = 'fh', filename= sector +'.csv')['symbol'].values
+        data = us_equity_daily_data_load(symbols = symbols, start_date = self.start_date,
+                                          end_date = self.end_date, trade_option = 'market_capital', 
+                                          dir_option = 'xq')
+        df = pd.DataFrame(data)
+        df_sum = df.sum(axis=1)
+        index = df_sum * 1000 /df_sum.iloc[0]
+        dict_index[sector] = index
 
       return pd.DataFrame(dict_index)
 
   def ratio(self, days = 1):
      df = self.calc()
-     return df.pct_change(days).round(2) * 100 
+     return df.pct_change(days).round(3) * 100 
