716a8f698ca823a96b24199cd4bed8228c2b30c2
luozero@126.com <luozero@126.com>
2024-10-19 08:19:11 +0000

    change file name again

--- quant_free/FinML/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Utility functions. In particular Chapter20 code on Multiprocessing and Vectorization
-"""
-
-from quant_free.FinML.utils.fast_ewma import ewma
-from quant_free.FinML.utils.multiprocess import expand_call, lin_parts, mp_pandas_obj, nested_parts, process_jobs, process_jobs_, \
-    report_progress
---
+++ quant_free/finml/features/__init__.py
@@ -0,0 +1,2 @@
+# from quant_free.finml.features.fracdiff import get_weights, frac_diff, get_weights_ffd, frac_diff_ffd
+import quant_free.finml.features.microstructure
